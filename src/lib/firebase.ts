0f316e7d040e2f8107d10295c9f502901dd5636f
elilopes <liclopes@gmail.com>
2026-08-25 17:45:59 -0300

    Add Firebase initialization and Firestore export for search USSD/MMI codes

---
+++ src/lib/firebase.ts
@@ -0,0 +1,10 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import firebaseConfig from '../../firebase-applet-config.json';
+
+const app = initializeApp({
+  ...firebaseConfig,
+  databaseId: firebaseConfig.firestoreDatabaseId
+});
+
+export const db = getFirestore(app);
