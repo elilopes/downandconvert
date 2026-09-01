7f4a72ee0baf876c016e98943ea15df7f7d2ee0e
elilopes <liclopes@gmail.com>
2026-09-01 17:56:16 -0300

    Refactor Firebase initialization and exports

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,10 +1,8 @@
-import { initializeApp, getApps } from 'firebase/app';
-import { initializeFirestore } from 'firebase/firestore';
+import { initializeApp, getApps, getApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 import firebaseConfig from '../../firebase-applet-config.json';
 
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
-
-// Use initializeFirestore with experimentalForceLongPolling which often helps in restricted iframe environments
-export const db = initializeFirestore(app, {
-  experimentalForceLongPolling: true
-}, firebaseConfig.firestoreDatabaseId);
+export const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
+export const auth = getAuth(app);
